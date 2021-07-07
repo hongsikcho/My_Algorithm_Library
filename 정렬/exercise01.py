e70dcab542f2ecbe6bb94ceb7ecadb94d19eb7ab
hongsikcho <76928632+hongsikcho@users.noreply.github.com>
2021-07-07 17:03:56 +0900

    Create exercise01.py

---
+++ 정렬/exercise01.py
@@ -0,0 +1,40 @@
+# 정렬을 사용하는 기본적인 문제였다.
+
+# 내 코드:
+
+n = int(input())
+
+list = []
+
+for i in range(n):
+    list.append(int(input()))
+
+def my_sort():
+
+    for i in range(len(list)):
+        max_index = i
+        for j in range(i+1 , len(list)):
+            if list[max_index] <= list[j]:
+                max_index = j
+        list[max_index], list[i] = list[i], list[max_index]
+
+    print(list)
+
+my_sort()
+
+# 모범 작성 코드:
+
+n = int(input())
+
+array = []
+
+for i in range(n):
+  array.append(int(input))
+  
+ 
+array = sorted(array, reverse = True)
+
+for i in array:
+  print(i , end = " ")
+  
+ # 파이썬에서는 리스트 정렬로 sort,sorted를 제공하므로 이 메서드 사용 시 더 빠르게 작성할 수 있음. 꼭기억!
