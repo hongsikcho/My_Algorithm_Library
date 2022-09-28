ce53cec9f6591b49eb89826b7740907ace2883e1
hongsikcho <76928632+hongsikcho@users.noreply.github.com>
2022-09-28 13:24:03 +0900

    Update 올바른 괄호.py

--- 프로그래머스2단계/올바른 괄호.py
+++ 프로그래머스2단계/올바른 괄호.py
@@ -1,1 +1,16 @@
+def solution(s):
+    answer = True
+    array = ''
+    
+    for i in s:
+        array+=i
+        if array[-2:] == '()':
+            array = array[:-2]
+            
+    if array:
+        return False
+    else:
+        return True
+    
 
+    return True
