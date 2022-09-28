deafad9ef103cf740dc27bd6c7fab6ebaceeaf14
hongsikcho <76928632+hongsikcho@users.noreply.github.com>
2022-09-28 13:46:12 +0900

    Update JadenCase 문자열 만들기.py

--- 프로그래머스2단계/JadenCase 문자열 만들기.py
+++ 프로그래머스2단계/JadenCase 문자열 만들기.py
@@ -8,3 +8,28 @@ def solution(s):
         s[i]=s[i].capitalize()
     answer=' '.join(s)
     return answer
+
+
+#split()과 spilt(" ")은 차이가 있다.
+#전자는 공백 여러개를 하나로 취급, 후자는 공백이 여러개여도 하나만 공백으로 
+=====다른 풀이=====
+def solution(s):
+    answer = ''
+    s = s.split(" ")
+    
+    for a in s:
+        
+        if a =='': #공백 처리를 잘 해줄 것!
+            answer += ' '
+            continue
+            
+        b = a.lower()
+        if a[0].isalpha():
+            answer += (b[0].upper() +b[1:])
+            answer+=' '
+        else:
+            answer += b
+            answer += ' '
+            
+    
+    return answer[:-1]
